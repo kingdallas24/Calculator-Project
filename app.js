9a04ea5558086fd57e440a2ee945824a5defa0ad
Miles Biringanine <milesgabriel23@gmail.com>
2022-06-21 20:02:13 -0500

    Fixed bugs in operating keys

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ let equals = document.querySelector(".equals");
 let percent = document.querySelector(".percent");
 let decimal = document.querySelector(".decimal");
 let positiveNegative = document.querySelector(".positiveNegative");
-
+let equation = document.querySelector(".equation");
+let zero = document.querySelector(".zero");
 let value1;
 let value2;
 let operator;
+let multiply = document.querySelector(".multiply");
+
+if (value2 == undefined) {
+  equals.disabled = true;
+}
 
 let symActive = false;
 let negativeActive = false;
+zero.disabled = true;
+positiveNegative.disabled = true;
 
 const addNums = function (a, b) {
   return a + b;
@@ -44,14 +52,20 @@ function operate(val1, val2, operator) {
 
 calcNum.forEach((num1) => {
   num1.addEventListener("click", (e) => {
+    positiveNegative.disabled = false;
+    zero.disabled = false;
+
     if (symActive == false) {
       if (numDisplay.textContent == "0") {
+        equation.textContent = "";
         numDisplay.textContent = "";
       }
 
       value1 = parseFloat(
         (numDisplay.textContent += num1.textContent).replace(/,/g, "")
       );
+
+      equation.textContent = value1;
 
       let length = value1.toString().length;
       console.log(length);
@@ -77,6 +91,12 @@ calcNum.forEach((num1) => {
 
 symbol.forEach((sym) => {
   sym.addEventListener("click", (e) => {
+    symbol.forEach((sym) => {
+      sym.disabled = true;
+    });
+    positiveNegative.disabled = true;
+    symActive = true;
+
     decimal.disabled = false;
     calcNum.forEach((num) => {
       num.disabled = false;
@@ -84,23 +104,31 @@ symbol.forEach((sym) => {
     symActive = true;
     if (sym.textContent.includes("+")) {
       sym.classList.add("addActive");
+      sym.classList.remove("multiplyActive", "subtractActive", "divideActive");
       operator = "+";
     } else if (sym.textContent.includes("-")) {
       sym.classList.add("subtractActive");
+      sym.classList.remove("addActive", "subtractActive", "divideActive");
+
       operator = "-";
     } else if (sym.textContent.includes("X")) {
       sym.classList.add("multiplyActive");
+      sym.classList.remove("addActive", "subtractActive", "divideActive");
       operator = "*";
     } else if (sym.textContent.includes("÷")) {
       sym.classList.add("divideActive");
+      sym.classList.remove("multiplyActive", "addActive", "subtractActive");
       operator = "/";
     }
     numDisplay.textContent = "";
+    equation.textContent += " " + operator + " ";
   });
 });
 
 calcNum.forEach((num2) => {
   num2.addEventListener("click", (e) => {
+    equals.disabled = false;
+    positiveNegative.disabled = false;
     symbol.forEach((sym) => {
       sym.classList.remove(
         "multiplyActive",
@@ -114,6 +142,7 @@ calcNum.forEach((num2) => {
       value2 = parseFloat(
         (numDisplay.textContent += num2.textContent).replace(/,/g, "")
       );
+      equation.textContent += num2.textContent;
 
       let length = value2.toString().length;
       console.log(length);
@@ -138,6 +167,11 @@ calcNum.forEach((num2) => {
 });
 
 equals.addEventListener("click", (e) => {
+  equals.disabled = true;
+  symbol.forEach((sym) => {
+    sym.disabled = false;
+  });
+
   numDisplay.style.fontSize = "75%";
   calcNum.forEach((num) => {
     num.disabled = true;
@@ -156,17 +190,26 @@ equals.addEventListener("click", (e) => {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   });
-  value1 = finalResult;
+  equation.textContent += " " + "=" + " " + finalResult;
 
-  console.log(finalResult);
+  value1 = finalResult;
+  if (finalResult == 0) console.log(finalResult);
+  value2 = undefined;
 });
 
 clear.addEventListener("click", (e) => {
   numDisplay.style.fontSize = "75%";
+  positiveNegative.disabled = true;
+  symActive = false;
+  equals.disabled = true;
+  equation.textContent = "";
+
   calcNum.forEach((num) => {
     num.disabled = false;
   });
   symbol.forEach((sym) => {
+    sym.disabled = true;
+
     sym.classList.remove(
       "multiplyActive",
       "addActive",
@@ -174,15 +217,15 @@ clear.addEventListener("click", (e) => {
       "divideActive"
     );
   });
-  if (value1 == 0) {
+  if (value1 == undefined) {
     decimal.disabled = true;
   } else {
     decimal.disabled = false;
   }
-
   numDisplay.textContent = "0";
-  value1 = 0;
-  value2 = 0;
+  value1 = undefined;
+
+  value2 = undefined;
 });
 
 percent.addEventListener("click", (e) => {
@@ -203,31 +246,42 @@ positiveNegative.addEventListener("click", (e) => {
 
 function makeNegative() {
   positiveNegative.classList.toggle("negative");
+  equation.classList.toggle("negative");
 
-  if (symActive == false && positiveNegative.className.includes("negative")) {
-    value1 = "-" + value1;
+  if (
+    symActive == false &&
+    positiveNegative.className.includes("negative") &&
+    equation.className.includes("negative")
+  ) {
+    value1 = -+value1;
+
     numDisplay.textContent = value1;
+    equation.textContent = value1;
     return value1;
   } else if (
     symActive == false &&
-    !positiveNegative.className.includes("negative")
+    !positiveNegative.className.includes("negative") &&
+    !equation.className.includes("negative")
   ) {
-    value1 = value1.replace("-", "");
     numDisplay.textContent = value1;
+    equation.textContent = value1;
     return value1;
   } else if (
     symActive == true &&
-    positiveNegative.className.includes("negative")
+    positiveNegative.className.includes("negative") &&
+    equation.className.includes("negative")
   ) {
-    value2 = "-" + value2;
+    value2 = -+value2;
     numDisplay.textContent = value2;
+    equation.textContent = `${value1} ${operator} ${value2}`;
     return value2;
   } else if (
     symActive == true &&
-    !positiveNegative.className.includes("negative")
+    !positiveNegative.className.includes("negative") &&
+    !equation.className.includes("negative")
   ) {
-    value2 = value2.replace("-", "");
     numDisplay.textContent = value2;
+    equation.textContent = parseFloat(`${value1} ${operator} ${value2}`);
     return value2;
   }
 }
@@ -237,3 +291,8 @@ decimal.addEventListener("click", (e) => {
     decimal.disabled = true;
   }, 100);
 });
+
+function addSymbol() {
+  symbol.disabled = true;
+  equation.textContent += " " + operator + " ";
+}
